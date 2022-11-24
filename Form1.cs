2bbbed7492e5690fcb24e1f19d09f150f4546e44
Aysu İsgəndərli <115875690+Aysu1442a@users.noreply.github.com>
2022-11-24 22:34:47 +0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oyun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void xox(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if ( label1.Text == "X" )
+            {
+                btn.Text = "X";
+                btn.Enabled = false;
+                label1.Text = "O";
+            }
+            else
+            {
+                btn.Text = "O";
+                btn.Enabled = false;
+                label1.Text = "X";
+            }
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X")
+            {
+                MessageBox.Show("Qazanan : X ");
+                oyunson();
+
+            }
+
+            if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
+            {
+                MessageBox.Show("Qazanan : X ");
+                oyunson();
+
+            }
+            if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
+            {
+                MessageBox.Show("Qazanan : X ");
+                oyunson();
+
+            }
+
+            if (button1.Text == "X" && button4.Text == "X" && button7.Text == "X")
+            {
+                MessageBox.Show("Qazanan : X ");
+                oyunson();
+
+            }
+            if (button2.Text == "X" && button5.Text == "X" && button8.Text == "X")
+            {
+                MessageBox.Show("Qazanan : X ");
+                oyunson();
+
+            }
+            if (button3.Text == "X" && button6.Text == "X" && button9.Text == "X")
+            {
+                MessageBox.Show("Qazanan : X ");
+                oyunson();
+
+            }
+            if (button1.Text == "X" && button5.Text == "X" && button9.Text == "X")
+            {
+                MessageBox.Show("Qazanan : X ");
+                oyunson();
+
+            }
+            if (button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
+            {
+                MessageBox.Show("Qazanan : X ");
+                oyunson();
+
+            }
+
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X")
+            {
+                MessageBox.Show("Qazanan : X ");
+                oyunson();
+
+            }
+
+            if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
+            {
+                MessageBox.Show("Qazanan : X ");
+                oyunson();
+
+            }
+            if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
+            {
+                MessageBox.Show("Qazanan : X ");
+                oyunson();
+
+            }
+
+            if (button1.Text == "O" && button4.Text == "O" && button7.Text == "O")
+            {
+                MessageBox.Show("Qazanan : O ");
+                oyunson();
+
+            }
+            if (button2.Text == "O" && button5.Text == "O" && button8.Text == "O")
+            {
+                MessageBox.Show("Qazanan : O ");
+                oyunson();
+
+            }
+            if (button3.Text == "O" && button6.Text == "O" && button9.Text == "O")
+            {
+                MessageBox.Show("Qazanan : O ");
+                oyunson();
+
+            }
+            if (button1.Text == "O" && button5.Text == "O" && button9.Text == "O")
+            {
+                MessageBox.Show("Qazanan : O ");
+                oyunson();
+
+            }
+            if (button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
+            {
+                MessageBox.Show("Qazanan : O ");
+                oyunson();
+
+            }
+
+            if (button1.Text != "" && button2.Text != "" && button3.Text != "" && button4.Text != "" && button5.Text != "" && button6.Text != "" && button7.Text != "" && button8.Text != "" && button9.Text != "" )
+            {
+                MessageBox.Show("Heç heçə");
+                oyunson();
+
+            }
+
+
+
+
+
+        }
+        private void oyunson()
+        {
+            label1.Text = "X";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
